ca009f1129e21a842cb3af91cd9fc200139965af
PedroASA <m.amaral.hhs@gmail.com>
2021-05-04 15:19:43 -0300

    Working app?

--- capstone/settings.py
+++ capstone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS =  ['pedroasa-calendar-app.herokuapp.com', '.localhost', '127.0.0.1']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
