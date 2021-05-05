2185dcd5558b279180fb1827ee156b1c84124985
PedroASA <m.amaral.hhs@gmail.com>
2021-05-05 10:33:28 -0300

    Add debug mode

--- capstone/settings.py
+++ capstone/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG") == 'TRUE'
 
 ALLOWED_HOSTS =  ['pedroasa-calendar-app.herokuapp.com', '.localhost', '127.0.0.1']
 
